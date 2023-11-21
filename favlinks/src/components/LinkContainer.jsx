7f6cc443d1ccbe4e9bc7eb7c076b4c9d602dfba1
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-11-20 22:11:29 -0500

    step 1-9 done

---
+++ favlinks/src/components/LinkContainer.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import Table from './Table'
+import Form from './Form'
+
+function LinkContainer(){
+    const [favLinks, setFavLinks] = useState([]);
+
+    const handleRemove = (index) => {
+        /*
+            TODO - Create logic for setting the state to filter array and remove favLink at index
+        */
+        const updatedLinks = [...favLinks];
+        updatedLinks.splice(index, 1);
+        setFavLinks(updatedLinks);
+      }
+    
+      const handleSubmit = (favLink) => {
+        /*
+                TODO - Create logic to set state and add new favLink to favLinks array in state
+            */
+
+      }
+
+    return(
+        <div>
+            <h1>My Favorite Links</h1>
+            <p>Add a new link with a name and URL to the table! </p>
+            <Table linkData={favLinks} removeLink={() => handleRemove(index)}/>
+            <h1>Add New</h1>
+            <Form />
+        </div>
+    )
+
+}
+export default LinkContainer
